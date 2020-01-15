a28fff79cf1717e5eed9d39860db4c3eb6c93c02
Francisco Sevilla <fsevilla@gmail.com>
2020-01-15 15:58:13 -0600

    Added router navigate

--- src/app/photos/photo.service.ts
+++ src/app/photos/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,15 @@ import { HttpClient } from '@angular/common/http';
 export class PhotoService {
 
   constructor(private httpClient:HttpClient) { }
-  
+
   getPhotos(albumId:number):Promise<any> {
     const url = 'https://jsonplaceholder.typicode.com/photos?albumId='+albumId;
     return this.httpClient.get(url).toPromise();
   }
+
+  getPhotosObservable(albumId:number):Observable<any> {
+    const url = 'https://jsonplaceholder.typicode.com/photos?albumId='+albumId;
+    return this.httpClient.get(url);
+  }
+  
 }
--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { PhotoService } from './photo.service';
 
@@ -15,22 +15,51 @@ export class PhotosComponent implements OnInit {
 
   dataLoaded:boolean = false;
 
-  constructor(private photoService:PhotoService, private activatedRoute:ActivatedRoute) {
-    this.activatedRoute.params.subscribe(data => {
-      this.getPhotos(data.albumId);
-    });
-  }
+  private albumId:number;
 
-  ngOnInit() {
+  constructor(
+    private photoService:PhotoService,
+    private activatedRoute:ActivatedRoute,
+    private router:Router
+  ) {
     
   }
 
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.albumId = parseInt(params.albumId, 10);
+      this.getPhotosObservable();
+    });
+  }
+
   getPhotos(albumId) {
-    this.photoService.getPhotos(albumId).then(response => {
-      console.log('Fotos: ', response);
+    this.photoService.getPhotos(albumId)
+      .then(response => {
+        this.photos = response;
+        this.dataLoaded = true;
+      })
+      .catch(err => {
+        console.log('Error', err);
+      });
+  }
+
+  getPhotosObservable() {
+    this.photoService.getPhotosObservable(this.albumId).subscribe(response => {
       this.photos = response;
       this.dataLoaded = true;
+    }, err => {
+      console.log('Error', err);
     });
+  }
+
+  goNext() {
+    this.router.navigate(['../'+this.getNextId()], {
+      relativeTo: this.activatedRoute
+    })
+  }
+
+  getNextId() {
+    return this.albumId + 1;
   }
 
 }
